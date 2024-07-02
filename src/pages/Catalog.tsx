0478940c72601339a21f37b4334aa9beaf5737c9
JafirRehman <mjafirrehman@gmail.com>
2024-07-02 07:17:13 -0700

    adding catalog page

---
+++ src/pages/Catalog.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Catalog:React.FC = () => {
+
+    return (
+        <div>
+            <div className=' box-content bg-richblack-800 px-4'>
+                <div className='mx-auto flex min-h-[260px]  flex-col justify-center gap-4 '>
+                    <p className='text-sm text-richblack-300'>Home / Catalog / <span className='text-yellow-25'>hello</span> </p>
+                    <p className='text-3xl text-richblack-5'>hello</p>
+                    <p className='max-w-[870px] text-richblack-200'>
+                        hello
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Catalog
