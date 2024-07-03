fa03c6631edc8104a78a63cdd0a07221f2d0dfc1
JafirRehman <mjafirrehman@gmail.com>
2024-07-02 23:25:48 -0700

    adding update catalog page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Navbar from './components/common/Navbar';
+import Catalog from './pages/Catalog'
 
 const App:React.FC=()=> {
   return (
@@ -13,6 +14,7 @@ const App:React.FC=()=> {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/catalog" element={<Catalog />} />
       </Routes>
     </div>
   )
--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 const Catalog:React.FC = () => {
-
+    const params=useParams()
+    useEffect(()=>{
+        console.log(params)
+    })
     return (
-        <div>
-            <div className=' box-content bg-richblack-800 px-4'>
-                <div className='mx-auto flex min-h-[260px]  flex-col justify-center gap-4 '>
-                    <p className='text-sm text-richblack-300'>Home / Catalog / <span className='text-yellow-25'>hello</span> </p>
-                    <p className='text-3xl text-richblack-5'>hello</p>
-                    <p className='max-w-[870px] text-richblack-200'>
-                        hello
-                    </p>
+        <div className='text-white mt-[60px]'>
+            <div className='bg-[#18181B]'>
+                <div className='max-w-[1250px] min-h-[260px] py-20 w-[90%] mx-auto flex flex-col justify-center gap-6 '>
+                    <p className='text-2xl text-richblack-300'>Home/Catalog/<span className='text-yellow-25'>hello</span> </p>
+                    <p className='text-6xl text-richblack-5'>hello</p>
+                    <p className='text-2xl max-w-[870px] text-richblack-200 '>Blockchain is a shared, immutable ledger that facilitates the process of recording transactions and tracking assets in a business network. An asset can be tangible (a house, car, cash, land) or intangible (intellectual property, patents, copyrights, branding). Virtually anything of value can be tracked and traded on a blockchain network, reducing risk and cutting costs for all involved.</p>
                 </div>
             </div>
         </div>
